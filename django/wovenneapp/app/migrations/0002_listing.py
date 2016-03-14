c2246e4e63285d9a363a554bb4dce66730e0b023
OldGareBear <betheagary@gmail.com>
2016-03-14 18:24:55 -0400

    master: create Listing model
    Signed-off-by: OldGareBear <betheagary@gmail.com>

---
+++ django/wovenneapp/app/migrations/0002_listing.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        ('app', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Listing',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('title', models.CharField(max_length=256)),
+                ('description', models.CharField(max_length=1024)),
+                ('photo', models.ImageField(help_text=b'Min size (90 x 58)px', null=True, upload_to=b'static/app/img/listings/', blank=True)),
+                ('listing_type', models.ForeignKey(related_name='type', to='app.ListingType')),
+                ('poster', models.ForeignKey(related_name='listings', to=settings.AUTH_USER_MODEL)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+    ]
--- django/wovenneapp/app/models/listing.py
+++ django/wovenneapp/app/models/listing.py
@@ -4,21 +4,21 @@ from django.db import models
 import simplejson as json
 
 
-# class Listing(models.Model):
-#
-#     poster = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='listings')
-#     title = models.CharField(max_length=256)
-#     description= models.CharField(max_length=1024)
-#     photo = models.ImageField(upload_to=b'static/app/img/listings/', help_text='Min size (90 x 58)px', null=True, blank=True)
-#     listing_type = models.ForeignKey('ListingType', related_name='type')
-#
-#     def to_json(self):
-#         return json.dumps({
-#             'poster': self.poster,
-#             'title': self.title,
-#             'description': self.description,
-#             'type': self.listing_type
-#         })
+class Listing(models.Model):
+
+    poster = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='listings')
+    title = models.CharField(max_length=256)
+    description= models.CharField(max_length=1024)
+    photo = models.ImageField(upload_to=b'static/app/img/listings/', help_text='Min size (90 x 58)px', null=True, blank=True)
+    listing_type = models.ForeignKey('ListingType', related_name='type')
+
+    def to_json(self):
+        return json.dumps({
+            'poster': self.poster,
+            'title': self.title,
+            'description': self.description,
+            'type': self.listing_type
+        })
 
 
 class ListingType(models.Model):
